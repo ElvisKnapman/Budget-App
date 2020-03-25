6c364774f2e178a42ff9f62b0d9fc91485bce322
Elvis Knapman <elvis@elvisknapman.com>
2020-03-25 15:51:30 -0400

    :sparkles: added containers to DOMstrings, and more..
    fixed bug that wasn't selecting placeholder text. Was using 'html' over and over, changed to use 'newHtml' variable.. added code to insert HTML before the end of the selected element

--- js/app.js
+++ js/app.js
@@ -64,6 +64,8 @@ const UIController = (function () {
     inputDescription: ".add__description",
     inputValue: ".add__value",
     inputBtn: ".add__btn",
+    incomeContainer: ".income__list",
+    expensesContainer: ".expenses__list",
   };
 
   return {
@@ -76,22 +78,26 @@ const UIController = (function () {
     },
 
     addListItem: function (obj, type) {
-      let html, newHtml;
+      let html, newHtml, element;
       // create HTML string with placeholder text
 
       if (type === "inc") {
+        element = DOMstrings.incomeContainer;
         html =
           '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       } else if (type === "exp") {
+        element = DOMstrings.expensesContainer;
         html =
           '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
       }
 
       // Replace placeholder text with some actual data
       newHtml = html.replace("%id%", obj.id);
-      newHTML = html.replace("%description%", obj.description);
-      newHtml = html.replace("%value%", obj.value);
+      newHtml = newHtml.replace("%description%", obj.description);
+      newHtml = newHtml.replace("%value%", obj.value);
+
       // insert HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML("beforeend", newHtml);
     },
 
     getDOMstrings: function () {
@@ -125,7 +131,7 @@ const controller = (function (budgetCtrl, UICtrl) {
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
     // 3. Add the item to the UI
-
+    UICtrl.addListItem(newItem, input.type);
     // 4. Calculate the budget
 
     // 5. Display the budget
