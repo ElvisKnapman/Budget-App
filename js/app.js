595074fb8d1cd9ad2b13cd03bbe8e438fb53b7e0
Elvis Knapman <elvis@elvisknapman.com>
2020-03-26 22:42:48 -0400

    :sparkles: added more DOM strings and displayBudget method
    added some strings to the DOM strings object.. also wrote displayBudget method which updates the DOM with budget, income and expense data

--- js/app.js
+++ js/app.js
@@ -103,6 +103,10 @@ const UIController = (function () {
     inputBtn: ".add__btn",
     incomeContainer: ".income__list",
     expensesContainer: ".expenses__list",
+    budgetLabel: ".budget__value",
+    incomeLabel: ".budget__income--value",
+    expensesLabel: ".budget__expenses--value",
+    percentageLabel: ".budget__expenses--percentage",
   };
 
   return {
@@ -161,6 +165,21 @@ const UIController = (function () {
       fieldsArr[0].focus();
     },
 
+    displayBudget: function (obj) {
+      document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+      document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+      document.querySelector(DOMstrings.expensesLabel).textContent =
+        obj.totalExp;
+
+      if (obj.percentage > 0) {
+        document.querySelector(
+          DOMstrings.percentageLabel
+        ).textContent = `${obj.percentage}%`;
+      } else {
+        document.querySelector(DOMstrings.percentageLabel).textContent = "---";
+      }
+    },
+
     getDOMstrings: function () {
       return DOMstrings;
     },
@@ -190,7 +209,7 @@ const controller = (function (budgetCtrl, UICtrl) {
     const budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
-    console.log(budget);
+    UICtrl.displayBudget(budget);
   };
 
   const ctrlAddItem = () => {
